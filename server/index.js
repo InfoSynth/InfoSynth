5030f6c4e5f5358e7cec82dcb0848cc13650ff94
Seungwan Kang <albertruaz@gmail.com>
2024-01-05 20:48:35 +0900

    fix: 개인정보 파일 분리

--- server/index.js
+++ server/index.js
@@ -1,51 +1,46 @@
-const express = require('express');
-const path = require('path');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+
+var db_data = require("./private");
 
 const app = express();
 
 app.use(express.json());
-var cors = require('cors');
+var cors = require("cors");
 
 app.use(cors());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(8000, function () {
-  console.log('listening on 8000')
-}); 
+  console.log("listening on 8000");
+});
 
-var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'dbmssql12!!',
-    port:3306,
-    database: 'dating_app'
-    
-  });
+var con = mysql.createConnection(db_data);
 
-con.connect(function(err){
+con.connect(function (err) {
   if (err) throw err;
-  console.log('Connected');
-})
+  console.log("Connected");
+});
 
-app.get('/get', function(req, res){
-    var sql = 'select * from users';
-    con.query(sql, function(err, id, fields){
-      var user_id = req.params.id;
-      if(id){
-        var sql='select * from users'
-        con.query(sql, function(err, id, fields){
-          if(err){
-            console.log(err);
-          }else{
-            res.json(id);
-            console.log('users:', user_id);
-            console.log('users:', fields);
-          }
-        })
-      }
-    })
-  })
+app.get("/get", function (req, res) {
+  var sql = "select * from users";
+  con.query(sql, function (err, id, fields) {
+    var user_id = req.params.id;
+    if (id) {
+      var sql = "select * from users";
+      con.query(sql, function (err, id, fields) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json(id);
+          console.log("users:", user_id);
+          console.log("users:", fields);
+        }
+      });
+    }
+  });
+});
---
+++ server/private.js
@@ -0,0 +1,16 @@
+// const db_data = {
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "mysql1234",
+//   database: "test_db",
+// };
+
+const db_data = {
+  host: "localhost",
+  user: "root",
+  password: "dbmssql12!!",
+  port: 3306,
+  database: "dating_app",
+};
+
+module.exports = db_data;
