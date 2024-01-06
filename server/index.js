f768d4d1582a5bf514fa7fcc1cb26ded633e1778
Seungwan Kang <albertruaz@gmail.com>
2024-01-06 16:25:46 +0900

    fix: memebers crud 구현 수정

--- server/index.js
+++ server/index.js
@@ -39,19 +39,21 @@ app.get("/members", function (req, res) {
 });
 
 //리스트 추가 (회원가입)
-app.post("/members", function (request, response) {
-  var user_name = request.body.user_name;
-  var user_email = request.body.user_email;
-  var account_id = request.body.account_id;
-  var account_password = request.body.account_password;
+app.post("/members", function (req, res) {
+  var id = req.body.id;
+  var name = req.body.name;
+  var birth = req.body.birth;
+  var gender = req.body.gender;
+  var email = req.body.email;
+  var password = req.body.password;
 
   con.query(
-    "INSERT INTO users (user_name, user_email, account_id, account_password) VALUES(?,?,?,?)",
-    [user_name, user_email, account_id, account_password],
+    "INSERT INTO users (name, birth, gender, email, password) VALUES(?,?,?,?,?)",
+    [name, birth, gender, email, password],
     function (error, data) {
       if (error) {
-        console.log(error);
-        response.status(500).json({ error: "Internal Server Error" });
+        console.error("Database error:", error);
+        res.status(500).json({ error: "Internal Server Error" });
       } else {
         res.json({ result: "1" });
       }
@@ -59,12 +61,30 @@ app.post("/members", function (request, response) {
   );
 });
 
+// 멤버 1명 가져오기
+app.get("/members/:id", function (req, res) {
+  var id = req.params.id;
+
+  var sql = "select * from users where id = ?";
+  con.query(sql, [id], function (err, result) {
+    if (err) {
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      if (res.length === 0) {
+        res.status(404).json({ error: "Member not found" });
+      } else {
+        res.json(result[0]);
+      }
+    }
+  });
+});
+
 // 멤버 수정
 app.put("/members/:id", function (req, res) {
-  var memberId = req.params.id;
+  var id = req.params.id;
   var updatedData = req.body;
   var sql = "UPDATE users SET ? WHERE id = ?";
-  con.query(sql, [updatedData, memberId], function (err, result) {
+  con.query(sql, [updatedData, id], function (err, result) {
     if (err) {
       res.status(500).json({ error: "Internal Server Error" });
     } else {
@@ -94,50 +114,27 @@ app.delete("/members/:id", function (req, res) {
   });
 });
 
-// 멤버 1명 가져오기
-app.get("/members/each", function (req, res) {
-  var user_email = req.body.user_email;
-
-  var sql = "select * from users where user_email = ?";
-  con.query(sql, [user_email], function (err, result) {
-    if (err) {
-      res.status(500).json({ error: "Internal Server Error" });
-    } else {
-      if (res.length === 0) {
-        // 해당 ID의 멤버가 없는 경우 404 Not Found 응답을 보냅니다.
-        res.status(404).json({ error: "Member not found" });
-      } else {
-        res.json(result[0]);
-      }
-    }
-  });
-});
-
 // 멤버 확인
 app.post("/members/each", function (req, res) {
-  var user_name = req.body.user_name;
-  var user_email = req.body.user_email;
-  var account_id = req.body.account_id;
-  var account_password = req.body.account_password;
+  var name = req.body.name;
+  var birth = req.body.birth;
+  var gender = req.body.gender;
+  var email = req.body.email;
+  var password = req.body.password;
 
   con.query(
-    "SELECT * FROM users WHERE account_id = ? AND account_password = ?",
-    [account_id, account_password],
+    "SELECT * FROM users WHERE email = ? AND password = ?",
+    [email, password],
     function (error, data) {
       if (error) {
         res.status(500).json({ error: "Internal Server Error" });
       } else {
         if (data.length == 1) {
           res.json(data);
-          req.session.is_logined = true; // 세션 정보 갱신
-          req.session.nickname = username;
-          req.session.save(function () {
-            res.redirect(`/`);
-          });
         } else if (data.length >= 1) {
-          res.status(404).json({ message: "User not found" });
+          res.status(404).json({ message: "Too Many User" });
         } else {
-          res.json({ result: "0" });
+          res.status(404).json({ message: "No User" });
         }
       }
     }
