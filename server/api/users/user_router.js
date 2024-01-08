44a01e17dcd392f2fddd492ca6d09b63d1bde524
Seungwan Kang <albertruaz@gmail.com>
2024-01-08 17:09:58 +0900

    fix: 회원가입시 토큰 필요없게 구현

--- server/api/users/user_router.js
+++ server/api/users/user_router.js
@@ -10,7 +10,7 @@ const router = require("express").Router();
 
 const { checkToken } = require("../../auth/token_validation");
 
-router.post("/", checkToken, createUser);
+router.post("/", createUser);
 router.get("/", checkToken, getUsers);
 // router.get("/:id", checkToken, getUserByUserID);
 router.get("/:email", checkToken, getUserByUserEmail);
