577663a30e76820260781c3f07f6827115db7c58
Seungwan Kang <albertruaz@gmail.com>
2024-01-08 19:16:37 +0900

    fix: 회원가입 log찍히도록 임시 수정

--- server/api/users/user_controller.js
+++ server/api/users/user_controller.js
@@ -11,10 +11,10 @@ const { sign } = require("jsonwebtoken");
 module.exports = {
   createUser: (req, res) => {
     const body = req.body;
-
+    console.log("controller");
+    console.log(body);
     const salt = genSaltSync(10);
     body.password = hashSync(body.password, salt);
-
     create(body, (err, results) => {
       if (err) {
         console.log(err);
