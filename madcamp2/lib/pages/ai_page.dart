45fc49471958dfe32f3f08a512927fbfffe9f4d3
Jiyeon Na <skwldus@hanyang.ac.kr>
2024-01-10 14:23:30 +0900

    add: url 보내기

--- madcamp2/lib/pages/ai_page.dart
+++ madcamp2/lib/pages/ai_page.dart
@@ -5,6 +5,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:madcamp2/models/response_model.dart';
 import 'package:madcamp2/server/network.dart';
+import 'package:snippet_coder_utils/FormHelper.dart';
+import 'package:snippet_coder_utils/ProgressHUD.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class AIPage extends StatefulWidget {
@@ -21,12 +23,15 @@ class _AIPageState extends State<AIPage> {
   String responseTxt =
       ''; // updated with the data which we get from the ai model
   late ResponseModel _responseModel;
+  String url = '';
+  String script = '';
+  bool isAPIcallProcess = false;
+  GlobalKey<FormState> globalFormKey = GlobalKey<FormState>();
 
   Network network = Network();
 
   @override
   void initState() {
-    // promptController = TextEditingController();
     super.initState();
   }
 
@@ -40,47 +45,123 @@ class _AIPageState extends State<AIPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: ListView.separated(
-              padding: const EdgeInsets.all(8),
-              itemCount: newsList.length,
-              itemBuilder: (BuildContext context, int index) {
-                // Use GestureDetector or InkWell for tap functionality
-                return GestureDetector(
-                  onTap: () {
-                    // Handle tap action here, for example, open a link
-                    String link = newsList[index]['link'];
-                    // Navigate to the link or perform other actions
-                    // print("Opening link: $link");
-                    launch('$link', forceSafariVC: true);
-                  },
+      body: ProgressHUD(
+        child: Form(
+          key: globalFormKey,
+          child: _aiUI(context),
+        ),
+        inAsyncCall: isAPIcallProcess,
+        opacity: 0.3,
+        key: UniqueKey(),
+      ),
+    );
+  }
+
+  Widget _aiUI(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 80.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: SizedBox(
+                  height: 45,
+                  child: FormHelper.inputFieldWidget(
+                    context,
+                    "url",
+                    "url을 입력해주세요.",
+                    (onValidateVal) {
+                      if (onValidateVal.isEmpty) {
+                        return 'url을 입력해주세요.';
+                      }
+
+                      if (validateYouTubeUrl(onValidateVal) != null) {
+                        return validateYouTubeUrl(onValidateVal);
+                      }
+
+                      return null;
+                    },
+                    (onSavedVal) {
+                      url = onSavedVal;
+                    },
+                    borderFocusColor: Colors.black,
+                    prefixIconColor: Colors.black,
+                    borderColor: Colors.black,
+                    textColor: Colors.black,
+                    hintColor: Colors.black.withOpacity(0.7),
+                    borderRadius: 10,
+                    showPrefixIcon: true,
+                    prefixIcon: Icon(Icons.link),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 10.0),
+                child: SizedBox(
+                  width: 75,
+                  height: 45,
+                  child: FormHelper.submitButton(
+                    "검색",
+                    () async {
+                      if (validateAndSave()) {
+                        var scriptMap = await network.sendUrl(url);
+                        setState(() {
+                          script = scriptMap['data'];
+                        });
+                      }
+                    },
+                    btnColor: Colors.white,
+                    borderColor: Colors.black,
+                    txtColor: Colors.black,
+                    borderRadius: 10,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Expanded(
+          child: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: newsList.length,
+            itemBuilder: (BuildContext context, int index) {
+              // Use GestureDetector or InkWell for tap functionality
+              return GestureDetector(
+                onTap: () {
+                  // Handle tap action here, for example, open a link
+                  String link = newsList[index]['link'];
+                  // Navigate to the link or perform other actions
+                  launch('$link', forceSafariVC: true);
+                },
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 5.0,
+                  margin: const EdgeInsets.all(8.0),
                   child: Container(
                     height: 50,
                     color: Colors.white,
                     child: Center(child: Text('${newsList[index]['title']}')),
                   ),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(),
-            ),
+                ),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
           ),
-          Expanded(child: PromptBldr(responseTxt: responseTxt)),
-          TextFormFieldBldr(
-            promptController: promptController,
-            btnFun: completionFun,
-          ),
-        ],
-      ),
+        ),
+        Expanded(child: PromptBldr(responseTxt: responseTxt)),
+      ],
     );
   }
 
   completionFun() async {
-    setState(() => responseTxt = 'Loading...');
-
     final response =
         await http.post(Uri.parse('https://api.openai.com/v1/completions'),
             headers: {
@@ -90,7 +171,7 @@ class _AIPageState extends State<AIPage> {
             body: utf8.encode(jsonEncode({
               "model": "gpt-3.5-turbo-instruct",
               "prompt": '"' +
-                  promptController.text +
+                  script +
                   '" read the given script and give only 3 important proper nouns in Korean. Just give me 3 words separated by commas.  ',
               "max_tokens": 50,
               "temperature": 0,
@@ -116,63 +197,24 @@ class _AIPageState extends State<AIPage> {
       newsList = news['data'];
     });
   }
-}
 
-class TextFormFieldBldr extends StatelessWidget {
-  const TextFormFieldBldr(
-      {super.key, required this.promptController, required this.btnFun});
+  bool validateAndSave() {
+    final form = globalFormKey.currentState;
+    if (form!.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
 
-  final TextEditingController promptController;
-  final Function btnFun;
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 50),
-        child: Row(children: [
-          Flexible(
-            child: TextFormField(
-              cursorColor: Colors.black,
-              controller: promptController,
-              autofocus: true,
-              style: const TextStyle(color: Colors.black, fontSize: 20),
-              decoration: InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.white,
-                  ),
-                  borderRadius: BorderRadius.circular(5.5),
-                ),
-                enabledBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.white,
-                  ),
-                ),
-                filled: true,
-                fillColor: Colors.white,
-                hintText: 'Ask me anything...',
-                hintStyle: const TextStyle(color: Colors.grey),
-              ),
-            ),
-          ),
-          Container(
-            color: Colors.black12,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: IconButton(
-                icon: const Icon(Icons.send),
-                color: Colors.white,
-                onPressed: () {
-                  btnFun();
-                },
-              ),
-            ),
-          ),
-        ]),
-      ),
-    );
+  String? validateYouTubeUrl(String value) {
+    String pattern =
+        r'^https:\/\/www\.youtube\.com\/watch\?v=[\w-]+$'; // YouTube video URL format
+    RegExp regex = RegExp(pattern);
+    if (!regex.hasMatch(value)) {
+      return 'The URL is not a valid YouTube video URL.';
+    }
+    return null;
   }
 }
 
